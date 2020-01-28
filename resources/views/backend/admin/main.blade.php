acb57aa3be4075c39709d5ee1ceceaec027876d0
fayyaztech <fayyaztech@gmail.com>
2020-01-28 23:56:16 +0530

    Logout added

--- resources/views/backend/admin/main.blade.php
+++ resources/views/backend/admin/main.blade.php
@@ -158,7 +158,8 @@
                                 </div>
                                 <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
                                 <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a>
-                                <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a>
+                            <a class="dropdown-item" href="#" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"><i class="fas fa-power-off mr-2"></i>Logout</a>
                             </div>
                         </li>
                     </ul>
@@ -208,7 +209,9 @@
         <!-- end wrapper  -->
         <!-- ============================================================== -->
 
-
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
     </div>
     <!-- ============================================================== -->
     <!-- end main wrapper  -->
