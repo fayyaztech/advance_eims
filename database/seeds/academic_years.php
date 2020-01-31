9ca597f89a6f6bb497f2b9bd52b2d51d5d7127c7
fayyaztech <fayyaztech@gmail.com>
2020-01-31 19:59:00 +0530

    active academic year seeder

--- database/seeds/academic_years.php
+++ database/seeds/academic_years.php
@@ -16,6 +16,7 @@ class academic_years extends Seeder
             "name"=>"2019-20",
             "start_date"=> Carbon::createFromFormat('Y-m-d', '2019-06-07')->toDateTimeString(),
             "end_date"=> Carbon::createFromFormat('Y-m-d', '2020-12-31')->toDateTimeString(),
+            "is_active"=>true,
             "created_at" => Carbon::now(),
         ]);
         DB::table("academic_years")->insert([
